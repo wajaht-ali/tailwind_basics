282f3e42a43cd7fd0285c21ffae2154e21a3f824
Wajahat-404 <109886908+Wajahat-404@users.noreply.github.com>
2023-07-22 14:30:09 +0500

    navbar-adjustments

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,11 +1,40 @@
 import React from 'react'
+import { AiOutlineMenu, AiOutlineClose } from "react-icons/ai";
+import { useState } from "react";
 
 const Header = () => {
-  return (
-    <div className= "bg-[#2699fb] p-4">
-        Header
-    </div>
-  )
+    const [toggle, setToggle] = useState(false);
+    return (
+        <div className="bg-[#2699fb] p-4">
+            <div className="max-w-[1240px] py-[10px] items-center  flex justify-between mx-auto">
+                <div className="text-3xl font-bold">
+                    Wajahat Ali
+                </div>
+                {
+                    toggle ?
+                        <AiOutlineClose onClick={() => setToggle(!toggle)} className='text-white text-2xl md:hidden block' />
+                        :
+                        <AiOutlineMenu onClick={() => setToggle(!toggle)} className='text-white text-2xl md:hidden block' />
+                }
+
+                <ul className='hidden md:flex text-white gap-5'>
+                    <li>Home</li>
+                    <li>Company</li>
+                    <li>Resources</li>
+                    <li>About</li>
+                    <li>Contact</li>
+                </ul>
+                {/* RESPONSIVENESS MENU */}
+                <ul className={" md:hidden w-full h-screen text-white fixed bg-black top-[88px] ${toggle ? 'left-[0]' : 'left-[-100%]'} "}>
+                <li className='p-3'>Home</li>
+                <li className='p-3'>Company</li>
+                <li className='p-3'>Resources</li>
+                <li className='p-3'>About</li>
+                <li className='p-3'>Contact</li>
+            </ul>
+        </div>
+        </div >
+    )
 }
 
 export default Header
